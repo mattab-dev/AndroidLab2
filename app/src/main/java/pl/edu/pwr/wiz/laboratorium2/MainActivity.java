08f49e68ad74cb39b9add8a125a640b031e9bf64
mateusz_tabor <mattab.dev@gmail.com>
2019-03-17 13:32:46 +0100

    complete project

--- app/src/main/java/pl/edu/pwr/wiz/laboratorium2/MainActivity.java
+++ app/src/main/java/pl/edu/pwr/wiz/laboratorium2/MainActivity.java
@@ -10,13 +10,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,6 +29,7 @@ import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
